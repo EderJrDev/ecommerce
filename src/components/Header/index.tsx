d9713ee0bd7588e5ee2793fa6d6d96daa34c6e3e
EderJrDev <ederjuninho2003@gmail.com>
2025-05-13 19:14:45 -0300

    feat: add new project print

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,14 +15,14 @@ const Header: React.FC = () => {
         <input
           type="text"
           placeholder="O que você está procurando?"
-          className="w-full md:w-96 p-2 rounded-full text-gray-500"
+          className="w-full md:w-96 p-2 rounded-full text-gray-800 placeholder:text-gray-700"
         />
       </div>
       <div className="flex items-center space-x-4 w-full md:w-auto justify-between md:justify-start">
-        <button className="flex items-center space-x-2 text-white border border-gray-800 rounded-full px-4 py-2 mb-2 md:mb-0 w-full md:w-auto">
+        <button className="flex items-center space-x-2 text-gray-800 border border-gray-800 rounded-full px-4 py-2 mb-2 md:mb-0 w-full md:w-auto">
           <span>Entre em Contato</span>
         </button>
-        <button className="flex items-center justify-center space-x-2 text-white border border-gray-800 rounded-full px-4 py-2 mb-2 md:mb-0 w-full md:w-auto">
+        <button className="flex items-center justify-center space-x-2 text-gray-800 border border-gray-800 rounded-full px-4 py-2 mb-2 md:mb-0 w-full md:w-auto">
           <span>Seu Pedido</span>
         </button>
       </div>
